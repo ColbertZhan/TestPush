f8d696330c60c65a9ab20df71933161c91286043
Colbert Zhan <2445762544@qq.com>
2017-05-22 20:51:04 +0800

    创建项目

---
+++ TestPush/TestPush/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestPush
+//
+//  Created by Colbert Zhan on 2017/5/22.
+//  Copyright © 2017年 Colbert Zhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
