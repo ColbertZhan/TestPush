7d9d46c815d6bf6345ea13cd6d2d91e37d5abbf4
Colbert Zhan <2445762544@qq.com>
2017-05-24 17:34:33 +0800

    添加管理第三方库

--- TestPush/TestPush/ViewController.m
+++ TestPush/TestPush/ViewController.m
@@ -6,6 +6,14 @@
 //  Copyright © 2017年 Colbert Zhan. All rights reserved.
 //
 
+
+/** NSLog 输出宏*/
+#ifdef DEBUG
+#define NSLog(FORMAT, ...) fprintf(stderr,"%s:%d\t%s\n",[[[NSString stringWithUTF8String:__FILE__] lastPathComponent] UTF8String], __LINE__, [[NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String]);
+#else
+#define NSLog(...)
+#endif
+
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -17,6 +25,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"aaa");
+    
 }
 
 
